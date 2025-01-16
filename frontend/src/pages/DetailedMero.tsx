75b2a6f22a7bff8df6a2108c6639efa2319d2892
enemyare <mishasultanov4@gmail.com>
2025-01-16 06:25:12 +0500

    Добавил запрос на сводку

--- frontend/src/pages/DetailedMero.tsx
+++ frontend/src/pages/DetailedMero.tsx
@@ -14,9 +14,9 @@ const DetailedMero = () => {
   const mero: ICreateForm  = data
   const periods:  IPeriods = mero?.periods[0]
   const {date, time} = useFormattedDate(periods?.startTime)
-  const {data: dataListUsers, error: errorListUsers} = useSWR(`/api/Mero/phorm-answer/get-list-by-mero`, getRequest)
-
-  if (error) return <>хзуй</>
+  const {data: dataListUsers, error: errorListUsers} = useSWR(`/api/Mero/phorm-answer/get-csv-for-mero?meroId=${mero.id}`, getRequest)
+  console.log(dataListUsers)
+  if (error) return <>Ошибка</>
 
   return (
     <>
